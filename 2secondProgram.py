44fa88f9116a262502707623165805ba0f66c99b
suryachambyal <chambyal.surya@gmail.com>
2025-01-20 12:03:04 +0530

    add second program

---
+++ 2secondProgram.py
@@ -0,0 +1,6 @@
+# Hey this is a comment. [ctrl + / ---> commenting multiple lines at once.]
+print("It's 22 December today \n and weather is cloudy and chilly.")
+print("It's \"22 December\" today \n and weather is cloudy and chilly.")
+print("Hey",30,2700)
+print("Hey",30,2700 , sep=":") #this is a "separator" which prints the result with character displayed in it
+# alt + shift + down arrow ---> it replicates the above written line
