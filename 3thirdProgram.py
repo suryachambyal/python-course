84a12601a6fe0311f11e405fc8f3d669203ef38a
suryachambyal <chambyal.surya@gmail.com>
2025-01-20 12:08:27 +0530

    add third program

---
+++ 3thirdProgram.py
@@ -0,0 +1,10 @@
+# variables and datatypes
+# string ---> str()
+# integer ---> int()
+# floating number ---> float()
+# boolean (true/false) ---> bool()
+name = "Surya"
+age = 30
+isAdult = True
+print(name)
+print(name,age,isAdult)
