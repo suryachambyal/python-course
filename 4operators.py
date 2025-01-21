338ee9e4cda282004dbb338a52fabcfbba0a1939
suryachambyal <chambyal.surya@gmail.com>
2025-01-21 15:05:36 +0530

    add operators file

---
+++ 4operators.py
@@ -0,0 +1,28 @@
+#addition
+a = 18
+b = 45
+print(a+b)
+#substraction
+a = 45
+b = 18
+print(a-b)
+#multiplication
+a = 18
+b = 45
+print(a*b)
+#divison
+a = 18
+b = 6
+print(a/b)
+#floordivison-this will return only the value before decimal
+a = 45
+b = 18
+print(a//b)
+#modulus-this will return the remainder value
+a = 18
+b = 5
+print(a%b)
+#exponential-this will return "a" multiplied "b" times
+a = 5
+b = 3
+print(a**b)
