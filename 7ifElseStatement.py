d3b0f46b2bebfd087174e975de5e0a02cb026b6c
suryachambyal <chambyal.surya@gmail.com>
2025-02-02 12:21:20 +0530

    add if else file

---
+++ 7ifElseStatement.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your age: "))
+if (a>=18):
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+num = int(input("Enter your number: "))
+if(num < 0):
+    print("number is NEGATIVE")
+elif(num == 0):
+    print("number is ZERO")
+else:
+    print("number is POSITIVE")    
